d7e15f233d160b2e37cc3e46cd7fccfc9018efb3
Pavanyata999 <pavanyata123@>
2024-09-27 09:44:49 +0530

    first commit

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,42 @@
+module.exports.homelist = function (req, res) {
+    res.render('locations-list', {
+        title: 'Travel Buddy - find your destinations!',
+        pageHeader: {
+            title: 'Travel Buddy',
+            strapline: 'Hidden Gems Near You!',
+            logo: '/images/logo.jpg'  
+        },
+        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about.Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+        locations: [{
+            name: 'Munnar',
+            address: 'Land of nature, South Indian',
+            rating: 4,
+            facilities: ['Traditional Cuisine', 'Scenic Views', 'Cultural Experience'],
+            distance: '1000km'
+        }, {
+            name: 'Kanya Kumari',
+            address: 'Land of varied topography',
+            rating: 4,
+            facilities: ['Beaches', 'Sunset Views', 'Historical Landmarks'],
+            distance: '1200km'
+        }, {
+            name: 'Gokarna',
+            address: 'Varanasi of the South',
+            rating: 5,
+            facilities: ['Pristine Beaches', 'Temples', 'Yoga Retreats'],
+            distance: '1250Km'
+        }]
+    });
+};
+module.exports.locationInfo1 = function (req, res) {
+    res.render('locations-info1', { title: 'Munnar' });
+};
+module.exports.locationInfo2 = function (req, res) {
+    res.render('locations-info2', { title: 'Kanya Kumari' });
+};
+module.exports.locationInfo3 = function (req, res) {
+    res.render('locations-info3', { title: 'Gokarna' });
+};
+module.exports.addReview = function (req, res) {
+    res.render('locations-review-form', { title: 'ADD REVIEW' });
+};
